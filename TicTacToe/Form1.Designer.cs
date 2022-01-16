270356f4cb1ae476411081df74b68a1d71961f48
Tamas <borbelytam9304@gmail.com>
2022-01-16 10:24:34 +0100

    TicTacToe, all files

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_NewGame = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btn_Reset = new System.Windows.Forms.Button();
+            this.lbl_player1 = new System.Windows.Forms.Label();
+            this.lbl_player2 = new System.Windows.Forms.Label();
+            this.lbl_player1Score = new System.Windows.Forms.Label();
+            this.lbl_player2Score = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_NewGame
+            // 
+            this.btn_NewGame.Location = new System.Drawing.Point(578, 53);
+            this.btn_NewGame.Name = "btn_NewGame";
+            this.btn_NewGame.Size = new System.Drawing.Size(150, 50);
+            this.btn_NewGame.TabIndex = 0;
+            this.btn_NewGame.Text = "New Game";
+            this.btn_NewGame.UseVisualStyleBackColor = true;
+            this.btn_NewGame.Click += new System.EventHandler(this.btn_NewGame_Click);
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Location = new System.Drawing.Point(578, 165);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(150, 50);
+            this.btn_Exit.TabIndex = 0;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(524, 397);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(248, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(524, 426);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(248, 23);
+            this.textBox2.TabIndex = 2;
+            // 
+            // btn_Reset
+            // 
+            this.btn_Reset.Location = new System.Drawing.Point(578, 109);
+            this.btn_Reset.Name = "btn_Reset";
+            this.btn_Reset.Size = new System.Drawing.Size(150, 50);
+            this.btn_Reset.TabIndex = 0;
+            this.btn_Reset.Text = "Reset";
+            this.btn_Reset.UseVisualStyleBackColor = true;
+            this.btn_Reset.Click += new System.EventHandler(this.btn_Reset_Click);
+            // 
+            // lbl_player1
+            // 
+            this.lbl_player1.AutoSize = true;
+            this.lbl_player1.Font = new System.Drawing.Font("Times New Roman", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_player1.Location = new System.Drawing.Point(427, 244);
+            this.lbl_player1.Name = "lbl_player1";
+            this.lbl_player1.Size = new System.Drawing.Size(151, 37);
+            this.lbl_player1.TabIndex = 3;
+            this.lbl_player1.Text = "Player 1 :";
+            // 
+            // lbl_player2
+            // 
+            this.lbl_player2.AutoSize = true;
+            this.lbl_player2.Font = new System.Drawing.Font("Times New Roman", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_player2.Location = new System.Drawing.Point(427, 293);
+            this.lbl_player2.Name = "lbl_player2";
+            this.lbl_player2.Size = new System.Drawing.Size(151, 37);
+            this.lbl_player2.TabIndex = 3;
+            this.lbl_player2.Text = "Player 2 :";
+            // 
+            // lbl_player1Score
+            // 
+            this.lbl_player1Score.AutoSize = true;
+            this.lbl_player1Score.Font = new System.Drawing.Font("Times New Roman", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_player1Score.Location = new System.Drawing.Point(578, 244);
+            this.lbl_player1Score.Name = "lbl_player1Score";
+            this.lbl_player1Score.Size = new System.Drawing.Size(34, 37);
+            this.lbl_player1Score.TabIndex = 3;
+            this.lbl_player1Score.Text = "0";
+            // 
+            // lbl_player2Score
+            // 
+            this.lbl_player2Score.AutoSize = true;
+            this.lbl_player2Score.Font = new System.Drawing.Font("Times New Roman", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_player2Score.Location = new System.Drawing.Point(578, 293);
+            this.lbl_player2Score.Name = "lbl_player2Score";
+            this.lbl_player2Score.Size = new System.Drawing.Size(34, 37);
+            this.lbl_player2Score.TabIndex = 3;
+            this.lbl_player2Score.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_player2);
+            this.Controls.Add(this.lbl_player2Score);
+            this.Controls.Add(this.lbl_player1Score);
+            this.Controls.Add(this.lbl_player1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_Reset);
+            this.Controls.Add(this.btn_NewGame);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_NewGame;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn_Reset;
+        private System.Windows.Forms.Label lbl_player1;
+        private System.Windows.Forms.Label lbl_player2;
+        private System.Windows.Forms.Label lbl_player1Score;
+        private System.Windows.Forms.Label lbl_player2Score;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        int player1 = 0;
+        int player2 = 0;
+        bool turn = true;
+        List<Button> buttons = new List<Button>();
+        
+        public void Button(int x)
+        {
+            
+
+            int btnFontSize = 30;
+            int top = -50;
+            int left = 50;
+            int btn = 1;
+            string btnName = "btn_";
+            
+
+            for (int i = 0; i < x; i++)
+            {
+                
+                left = 50;
+                top += 105;
+                
+                for (int j = 0; j < x; j++)
+                {
+                    Button newbutton = new Button();
+                    newbutton.Name = btnName + btn;
+                    newbutton.Font = new Font(newbutton.Font.FontFamily, btnFontSize);
+                    newbutton.Text = "";
+                    newbutton.Top = top;
+                    newbutton.Left = left;
+                    newbutton.Size = new Size(100, 100);
+                    this.Controls.Add(newbutton);
+                    newbutton.Click += new EventHandler(newbutton_Click);
+                    left += newbutton.Width + 5;
+                    btn++;
+                    buttons.Add(newbutton);
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Visible = false; // Ki kommentelve ezt a sort, nyomon követhetjük a textbox alakulását.
+            textBox2.Visible = false; // -""-
+            btn_Reset.Enabled = false;
+        }
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_NewGame_Click(object sender, EventArgs e)
+        {
+            Reset();
+            lbl_player1Score.Text = "0";
+            lbl_player2Score.Text = "0";
+            btn_Reset.Enabled = true;
+            Button(3);
+            
+        }
+
+        private void newbutton_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            if (turn)
+            {
+                button.Text = "X";
+                turn = false;
+                button.Enabled = false;
+            }
+            else
+            {
+                button.Text = "O";
+                turn = true;
+                button.Enabled = false;
+            }
+            if(button.Text == "X")
+            {
+                textBox1.Text += button.Name.ToString();
+            }
+            else
+            {
+                textBox2.Text += button.Name.ToString();
+            }
+            checkTheWinnerx();
+            
+        }
+        private void Reset()
+        {
+            foreach (var clearButtons in buttons)
+            {
+                clearButtons.Text = "";
+                clearButtons.Enabled = true;
+            }
+            textBox1.Text = "";
+            textBox2.Text = "";
+           
+        }
+        private void btn_Reset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+        private void checkTheWinnerx()
+        {
+            
+            if (textBox1.Text.Contains("btn_1") && textBox1.Text.Contains("btn_2") && textBox1.Text.Contains("btn_3"))
+            {
+                MessageBox.Show("X nyert!");
+                player1++;
+                lbl_player1Score.Text = player1.ToString();
+                
+            }
+            else if (textBox2.Text.Contains("btn_1") && textBox2.Text.Contains("btn_2") && textBox2.Text.Contains("btn_3"))
+            {
+                MessageBox.Show("O nyert!");
+                player2++;
+                lbl_player2Score.Text = player2.ToString();
+            }
+            else if (textBox1.Text.Contains("btn_1") && textBox1.Text.Contains("btn_5") && textBox1.Text.Contains("btn_9"))
+            {
+                MessageBox.Show("X nyert!");
+                player1++;
+                lbl_player1Score.Text = player1.ToString();
+            }
+            else if (textBox2.Text.Contains("btn_1") && textBox2.Text.Contains("btn_5") && textBox2.Text.Contains("btn_9"))
+            {
+                MessageBox.Show("O nyert!");
+                player2++;
+                lbl_player2Score.Text = player2.ToString();
+            }
+            else if (textBox1.Text.Contains("btn_1") && textBox1.Text.Contains("btn_4") && textBox1.Text.Contains("btn_7"))
+            {
+                MessageBox.Show("X nyert!");
+                player1++;
+                lbl_player1Score.Text = player1.ToString();
+            }
+            else if(textBox2.Text.Contains("btn_1") && textBox2.Text.Contains("btn_4") && textBox2.Text.Contains("btn_7"))
+            {
+                MessageBox.Show("O nyert!");
+                player2++;
+                lbl_player2Score.Text = player2.ToString();
+            }
+            else if (textBox1.Text.Contains("btn_2") && textBox1.Text.Contains("btn_5") && textBox1.Text.Contains("btn_8"))
+            {
+                MessageBox.Show("X nyert!");
+                player1++;
+                lbl_player1Score.Text = player1.ToString();
+            }
+            else if (textBox2.Text.Contains("btn_2") && textBox2.Text.Contains("btn_5") && textBox2.Text.Contains("btn_8"))
+            {
+                MessageBox.Show("O nyert!");
+                player2++;
+                lbl_player2Score.Text = player2.ToString();
+            }
+            else if (textBox1.Text.Contains("btn_3") && textBox1.Text.Contains("btn_6") && textBox1.Text.Contains("btn_9"))
+            {
+                MessageBox.Show("X nyert!");
+                player1++;
+                lbl_player1Score.Text = player1.ToString();
+            }
+            else if (textBox2.Text.Contains("btn_3") && textBox2.Text.Contains("btn_6") && textBox2.Text.Contains("btn_9"))
+            {
+                MessageBox.Show("O nyert!");
+                player2++;
+                lbl_player2Score.Text = player2.ToString();
+            }
+            else if (textBox1.Text.Contains("btn_3") && textBox1.Text.Contains("btn_5") && textBox1.Text.Contains("btn_7"))
+            {
+                MessageBox.Show("X nyert!");
+                player1++;
+                lbl_player1Score.Text = player1.ToString();
+            }
+            else if(textBox2.Text.Contains("btn_3") && textBox2.Text.Contains("btn_5") && textBox2.Text.Contains("btn_7"))
+            {
+                MessageBox.Show("O nyert!");
+                player2++;
+                lbl_player2Score.Text = player2.ToString();
+            }
+            else if (textBox1.Text.Contains("btn_4") && textBox1.Text.Contains("btn_5") && textBox1.Text.Contains("btn_6"))
+            {
+                MessageBox.Show("X nyert!");
+                player1++;
+                lbl_player1Score.Text = player1.ToString();
+            }
+            else if(textBox2.Text.Contains("btn_4") && textBox2.Text.Contains("btn_5") && textBox2.Text.Contains("btn_6"))
+            {
+                MessageBox.Show("O nyert!");
+                player2++;
+                lbl_player2Score.Text = player2.ToString();
+            }
+            else if (textBox1.Text.Contains("btn_7") && textBox1.Text.Contains("btn_8") && textBox1.Text.Contains("btn_9"))
+            {
+                MessageBox.Show("X nyert!");
+                player1++;
+                lbl_player1Score.Text = player1.ToString();
+            }
+            else if(textBox2.Text.Contains("btn_7") && textBox2.Text.Contains("btn_8") && textBox2.Text.Contains("btn_9"))
+            {
+                MessageBox.Show("O nyert!");
+                player2++;
+                lbl_player2Score.Text = player2.ToString();
+            }
+        }
+
+        
+    }
+}
